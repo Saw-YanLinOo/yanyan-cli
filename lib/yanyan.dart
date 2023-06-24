a7e08bb9adf91d8bb043fb8767ced2812ebaf8f1
Yan Yan <umgmobilegitone@gmail.com>
2023-06-24 13:23:39 +0630

    ✨ init project

---
+++ lib/yanyan.dart
@@ -0,0 +1,10 @@
+/// yanyan, A Very Good Project created by Very Good CLI.
+///
+/// ```sh
+/// # activate yanyan
+/// dart pub global activate yanyan
+///
+/// # see usage
+/// yanyan --help
+/// ```
+library yanyan;
